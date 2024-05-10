b79e46f27668bd35479acd831d562547b52d55c9
Olti Gjoni <oltigjoni@Oltis-MacBook-Air.local>
2024-05-10 19:32:17 -0400

    insertionsort commenting

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -9,13 +9,21 @@ InsertionSort.cpp
 #include "InsertionSort.hpp"
 
 void insertionSort(std::vector<int>& data, int left, int right) {
+    // Start from the second element in the subarray.
     for (int i = left + 1; i <= right; i++) {
+        // The 'key' is the value to be inserted into the sorted portion of the array.
         int key = data[i];
+        // Start comparing with the element just before the current element.
         int j = i - 1;
+
+        // Move elements of data[0..i-1], that are greater than key,
+        // to one position ahead of their current position.
+        // This loop will shift all elements that are greater than 'key' to the right by one position.
         while (j >= left && data[j] > key) {
             data[j + 1] = data[j];
-            j--;
+            j--; // Move back through the array.
         }
+        // Insert the 'key' in its correct location in the sorted portion.
         data[j + 1] = key;
     }
 }
